583234b95fa9a9a157cb4827bc869c00db1739b8
AndriusC <31574268+AndriusC@users.noreply.github.com>
2018-09-27 22:48:58 +0300

    Some changes to the HowToVerify embed

--- commands/howToVerify.js
+++ commands/howToVerify.js
@@ -4,9 +4,9 @@ const channels = require("../dataFiles/channels.json");
 module.exports.run = async (lanisBot, message, args) => {
     const securityRole = message.guild.roles.find(role => role.name === "Security");
     if (message.member.roles.highest.position < securityRole.position && !message.member.roles.find(role => role.name === "Verifier")) return await message.channel.send("You can not use this command as a non Security or Verifier.");
-    const faqChannel = lanisBot.channels.get(channels.howToVerify);
+    const howToVerifyChannel = lanisBot.channels.get(channels.howToVerify);
 
-    await faqChannel.messages.fetch().then(async messages => {
+    await howToVerifyChannel.messages.fetch().then(async messages => {
         for (const message of messages.values()) {
             await message.delete();
             console.log("deleted message");
@@ -17,7 +17,7 @@ module.exports.run = async (lanisBot, message, args) => {
         .setAuthor("A Tutorial On How To Verify")
         .addField(" ឵឵ ឵឵", "Firstly, read <#482394590721212416> and <#482368517568462868>")
 
-    await faqChannel.send(faqMessage);
+    await howToVerifyChannel.send(howToVerify);
 }
 
 module.exports.help = {
